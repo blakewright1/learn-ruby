2203dac4aca9462a867b4d1bc9e1969bee149b2d
blakewright1 <blakewright1021@gmail.com>
2020-04-26 13:46:21 -0400

    Menu written

--- PigLatin/piglatin.rb
+++ PigLatin/piglatin.rb
@@ -1,15 +1,5 @@
 #this program translates between english and pig-latin
 
-english = "words"
-puts english[1..-1] + english[0] + "ay"
-
-pig_latin = "ordsway"
-puts pig_latin[pig_latin.length()-3] + pig_latin[0, pig_latin.length()-3]
-
-
-
-
-
 #given a word in pig latin will return a word in english
 def pig_to_english(pig_latin)
   return pig_latin[pig_latin.length()-3] + pig_latin[0, pig_latin.length()-3]
@@ -19,3 +9,37 @@ end
 def english_to_pig(english)
   return english[1..-1] + english[0] + "ay"
 end
+
+puts "Welcome to the pig-latin <--> english translator"
+
+
+choice = 1
+while choice != 0      #when 0 is entered stop the program, otherwise continue
+  puts "Select one of the following options: "
+  puts "1) translate a word from english to pig-latin"
+  puts "2) translate a sentence from english to pig-latin"
+  puts "3) translate a word from pig-latin to english"
+  puts "4) translate a sentence from pig-latin to english"
+  puts "0) exit the program"
+  case choice
+  when 1
+    print "enter a word in english: "
+    word = gets.chomp
+    print english_to_pig(word)
+  when 2
+    print "enter a sentence in english: "
+    sentence = gets.chomp
+
+  when 3
+    print "enter a word in pig-latin: "
+    word = gets.chomp
+    print pig_to_english(word)
+  when 4
+    print "enter a sentence in pig_latin: "
+    sentence = gets.chomp
+  else
+    puts "invalid option, enter a number from the menu"
+  end
+
+end
+puts "Goodbye!"
